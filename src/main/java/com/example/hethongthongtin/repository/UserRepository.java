947dd48496b1a1ee9b7e4fd69c889e2d7090e68c
Minh Phạm Quang <minhkta1512@gmail.com>
2023-12-25 10:26:55 +0700

    test chuc nang generate 5 trieu ban ghi

--- src/main/java/com/example/hethongthongtin/repository/UserRepository.java
+++ src/main/java/com/example/hethongthongtin/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.hethongthongtin.repository;
 
+import com.example.hethongthongtin.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
